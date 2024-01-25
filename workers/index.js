2311624dd2ac9d87466cc35d20da59909236978f
AzureAD\OlkenMerxira <omerxira@unitsystem.net>
2024-01-25 19:24:41 +0100

    changes and BUG FIXING

--- workers/index.js
+++ workers/index.js
@@ -1,25 +1,36 @@
 import { Client, logger } from 'camunda-external-task-client-js';
+// import mysql from 'mysql';
+
+// var con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '12345',
+// });
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log('Connected!');
+// });
 
 const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger };
-
 const client = new Client(config);
 
+client.subscribe('ruan_cv', async function ({ task, taskService }) {
+  const cv = task.variables.get('cv');
+  // cv qe marrim nga aplikanti per ta ruajtur ne databaze
+  console.log(cv);
+  console.log('Duke ruajtur CV ne databaze...');
+  setTimeout(() => {
+    console.log(`${cv} u ruajt ne databaze`);
+  }, 1000);
+
+  await taskService.complete(task);
+});
+
 client.subscribe('dergo_feedback_finance', async function ({ task, taskService }) {
-  console.log('Duke printuar feedback ne lidhje me kandidatin tek departamenti i finances...');
+  console.log('Duke derguar feedback ne lidhje me kandidatin tek departamenti i finances...');
   setTimeout(() => {
     console.log('Feedback u dergua me sukses');
   }, 1000);
 
   await taskService.complete(task);
-  client.unsubscribe();
-});
-
-client.subscribe('ruan_cv', async function ({ task, taskService }) {
-  console.log('Duke ruajtur CV ne databaze...');
-  setTimeout(() => {
-    console.log('CV u ruajt ne databaze');
-  }, 1000);
-
-  await taskService.complete(task);
-  client.unsubscribe();
 });
